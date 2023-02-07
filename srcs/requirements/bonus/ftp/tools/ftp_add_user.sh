a3afccc5e580e95d253eaa489f842b22a0295979
hel-makh <hamzaelmakhroubi@gmail.com>
2023-02-07 20:46:13 +0100

    feat: added ftp service

---
+++ srcs/requirements/bonus/ftp/tools/ftp_add_user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+service vsftpd start
+
+adduser $FTP_USER
+
+FTP_USER_PW=$(expect -c "
+    set timeout 10
+    spawn passwd $FTP_USER
+    expect \"New password:\"
+    send \"$FTP_PW\r\"
+    expect \"Retype new password:\"
+    send \"$FTP_PW\r\"
+    expect eof
+")
+
+echo "$FTP_USER_PW"
+
+usermod -a -G www-data $FTP_USER
+
+chmod -R 775 /var/www/html
+
+echo $FTP_USER | tee -a /etc/vsftpd.userlist
+
+service vsftpd stop
+
+vsftpd
